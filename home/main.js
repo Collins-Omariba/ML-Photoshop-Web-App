426665120575a6785dd5ab045832d6d84b7046a5
Collins Omariba <collinsomariba@gmail.com>
2023-09-17 14:19:02 +0300

    Add js functions on button press

--- home/main.js
+++ home/main.js
@@ -2,41 +2,62 @@ function openCvReady() {
     cv['onRuntimeInitialized'] = () => {
         console.trace('cv ready')
 
-    
         let imgInput = cv.imread('img-main');
-        let imgGray = imgInput.clone();
-        let imgCanny = imgInput.clone();
-        let imgBlur = new cv.Mat();
-        let imgFilter = new cv.Mat();
 
-        
+        // RGB Button
+        document.getElementById("button-rgb").onclick = function () {
+            let imgInput = cv.imread('img-main');
+            cv.imshow('main-canvas', imgInput);
+            imgInput.delete();
+            console.log("rgb");
+        }
 
-        //convert the image to grayscale
-        cv.cvtColor(imgInput, imgGray, cv.COLOR_RGBA2GRAY, 0);
+        // GRAY Button
+        document.getElementById("button-gray").onclick = function () {
+            let imgInput = cv.imread('img-main');
+            let imgGray = imgInput.clone();
+            cv.cvtColor(imgInput, imgGray, cv.COLOR_RGBA2GRAY, 0);
+            cv.imshow('main-canvas', imgGray);
+            imgInput.delete();
+            imgGray.delete();
+        }
 
-        //blur the image
-        let ksize = new cv.Size(20, 20);
-        let anchor = new cv.Point(-1, -1);
-        cv.blur(imgInput, imgBlur, ksize, anchor, cv.BORDER_DEFAULT);
+        // BLUR Button
+        document.getElementById("button-blur").onclick = function () {
+            let imgInput = cv.imread('img-main');
+            let imgBlur = new cv.Mat();
+            let ksize = new cv.Size(20, 20);
+            let anchor = new cv.Point(-1, -1);
+            cv.blur(imgInput, imgBlur, ksize, anchor, cv.BORDER_DEFAULT);
+            cv.imshow('main-canvas', imgBlur);
+            imgInput.delete();
+            imgBlur.delete();
+        }
 
-        //filter the image
-        let M = cv.Mat.eye(3, 3, cv.CV_32FC1);
-        let anchor2 = new cv.Point(-1, -1);
-        cv.filter2D(imgInput, imgFilter, cv.CV_8U, M, anchor2, 0, cv.BORDER_DEFAULT);
+        // FILTER Button
+        document.getElementById("button-filter").onclick = function () {
+            let imgInput = cv.imread('img-main');
+            let imgFilter = new cv.Mat();
+            let M = cv.Mat.eye(3, 3, cv.CV_32FC1);
+            let anchor2 = new cv.Point(-1, -1);
+            cv.filter2D(imgInput, imgFilter, cv.CV_8U, M, anchor2, 0, cv.BORDER_DEFAULT);
+            cv.imshow('main-canvas', imgFilter);
+            imgInput.delete();
+            imgFilter.delete();
+            M.delete();
+        }
 
-        //find the edges
-        cv.Canny(imgInput, imgCanny, 50, 100);
+        // CANNY Button
+        document.getElementById("button-canny").onclick = function () {
+            let imgInput = cv.imread('img-main');
+            let imgCanny = new cv.Mat();
+            cv.Canny(imgInput, imgCanny, 50, 100);
+            cv.imshow('main-canvas', imgCanny);
+            imgInput.delete();
+            imgCanny.delete();
+        }
 
-       
-        cv.imshow('main-canvas', imgFilter);
+        cv.imshow('main-canvas', imgInput);
         imgInput.delete();
-        imgGray.delete();
-        imgBlur.delete();
-        imgCanny.delete();
-
-        imgFilter.delete();
-        M.delete();
-
-
     }
 }
